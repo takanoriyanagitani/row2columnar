f52dd25e0253f0adb16d5b14a073d5cf37b1d7df
yanagitani <takanori.yanagitani@gmail.com>
2020-03-08 09:50:16 +0900

    sum m256d

--- column_stat.py
+++ column_stat.py
@@ -36,3 +36,29 @@ def float2stat_default(stat=dict(), number=0.0, alt=0.0):
 
 def numbers2stat_float(numbers=iter([]), float2stat=float2stat_default, stat0=dict()):
   return reduce(float2stat, numbers, stat0)
+
+def float_add(a=0.0, b=0.0):
+  invalid_a = float != type(a) or math.isnan(a)
+  invalid_b = float != type(b) or math.isnan(b)
+  na = 0.0 if invalid_a else a
+  nb = 0.0 if invalid_b else b
+  return na + nb
+
+def m256d_add(a=(0.0,0.0,0.0,0.0), b=(0.0,0.0,0.0,0.0), z=(0.0,0.0,0.0,0.0)):
+  na = z if None == a else a
+  nb = z if None == b else b
+  return (
+    float_add(na[0], nb[0]),
+    float_add(na[1], nb[1]),
+    float_add(na[2], nb[2]),
+    float_add(na[3], nb[3]),
+  )
+
+def m256d2stat_default(stat=dict(), m256d=(0.0,0.0,0.0,0.0), alt=0.0):
+  stat["sum"] = m256d_add(stat.get("sum", (0,0,0,0,0,0,0,0)), m256d)
+  #stat["max"] = m256d_max(stat.get("sum", None), m256d)
+  #stat["min"] = m256d_min(stat.get("sum", None), m256d)
+  return stat
+
+def numbers2stat_m256d(d256bits=iter([]), m256d2stat=m256d2stat_default, stat0=dict()):
+  return reduce(m256d2stat, d256bits, stat0)
--- column_stat_test.py
+++ column_stat_test.py
@@ -48,3 +48,48 @@ def test_numbers2stat_float():
   assert 3.0 == a["sum"]
   assert 2.0 == a["max"]
   assert 0.0 == a["min"]
+
+def test_float_add():
+  nan = float("nan")
+  assert 0.0 == column_stat.float_add()
+  assert 0.0 == column_stat.float_add(None, None)
+  assert 0.0 == column_stat.float_add(nan, nan)
+  assert 0.0 == column_stat.float_add(0.0, None)
+  assert 0.0 == column_stat.float_add(0.0, nan)
+  assert 0.0 == column_stat.float_add(nan,  0.0)
+  assert 0.0 == column_stat.float_add(None, 0.0)
+  assert 1.0 == column_stat.float_add(None, 1.0)
+  assert 1.0 == column_stat.float_add(nan, 1.0)
+  assert 1.0 == column_stat.float_add(1.0, nan)
+  assert 1.0 == column_stat.float_add(1.0, None)
+  assert 3.0 == column_stat.float_add(1.0, 2.0)
+  assert 3.0 == column_stat.float_add(2.0, 1.0)
+
+def test_m256d_add():
+  nan = float("nan")
+  assert (0.0, 0.0, 0.0, 0.0) == column_stat.m256d_add()
+  assert (0.0, 0.0, 0.0, 0.0) == column_stat.m256d_add((0.0, 0.0, 0.0, 0.0))
+  assert (0.0, 0.0, 0.0, 0.0) == column_stat.m256d_add((0.0, 0.0, 0.0, 0.0), None)
+  assert (0.0, 0.0, 0.0, 0.0) == column_stat.m256d_add((nan, nan, nan, nan), None)
+  assert (0.0, 0.0, 0.0, 0.0) == column_stat.m256d_add(None, (0.0, 0.0, 0.0, 0.0))
+  assert (0.0, 0.0, 0.0, 0.0) == column_stat.m256d_add(None, (nan, nan, nan, nan))
+  assert (0.0, 1.0, 2.0, 3.0) == column_stat.m256d_add((0.0,1.0,2.0,3.0),(nan,nan,nan,nan))
+  assert (0.0, 1.0, 2.0, 3.0) == column_stat.m256d_add((nan,nan,nan,nan),(0.0,1.0,2.0,3.0))
+  assert (0.0, 1.0, 2.0, 3.0) == column_stat.m256d_add((0.0,0.5,1.0,1.5),(0.0,0.5,1.0,1.5))
+
+def test_numbers2stat_m256d():
+  nan = float("nan")
+  d256bits = iter([
+    (nan, 1.0, 2.0, 3.0),
+    (1.0, nan, 3.0, 4.0),
+    (2.0, 3.0, nan, 5.0),
+    None,
+    (3.0, 4.0, 5.0, nan),
+  ])
+  a = column_stat.numbers2stat_m256d(d256bits)
+  assert dict == type(a)
+  assert "sum" in a
+  s = a["sum"]
+  assert tuple == type(s)
+  assert 4 == len(s)
+  assert (6.0, 8.0, 10.0, 12.0) == s
